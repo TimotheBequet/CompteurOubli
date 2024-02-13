1121d37f75b841cebd03a01060dcdb8f494d470f
Tim <tim.bequet@gmail.com>
2024-02-13 21:56:02 +0100

    add environment modes

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Persons } from 'src/app/classes/persons';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-main',
@@ -9,15 +10,19 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MainComponent implements OnInit {
   lstPersons: Persons[] = [];
+  basePath: string = '';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    if (environment.name === 'PROD') {
+      this.basePath = '/CompteurOubli';
+    }
     this.initListePersons();
   }
 
   initListePersons(): void {
-    this.http.get<Persons[]>('/assets/persons.json').subscribe((datas: any) => {
+    this.http.get<Persons[]>(this.basePath + '/assets/persons.json').subscribe((datas: any) => {
       datas.map((data: any) => this.lstPersons.push(data));
       if (this.lstPersons.length) {
         this.lstPersons.sort(function(a, b) {
@@ -45,11 +50,11 @@ export class MainComponent implements OnInit {
 
   getUrlMedaille(index: number): string {
     if (index === 0) {
-      return "../../../assets/images/or.png";
+      return this.basePath + "/assets/images/or.png";
     } else if (index === 1) {
-      return "../../../assets/images/argent.png";
+      return this.basePath + "/assets/images/argent.png";
     } else if (index === 2) {
-      return "../../../assets/images/bronze.png";
+      return this.basePath + "/assets/images/bronze.png";
     } else {
       return "";
     }
